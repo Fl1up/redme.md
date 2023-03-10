972ce481f3e8e22b5e2d1631eadd7fae18133459
German <germanstarinenko@gmail.com>
2023-03-10 13:05:20 +0300

    add def

--- str_func.py
+++ str_func.py
@@ -1,3 +1,11 @@
-a = input()
-""" ввод чего-либо """
-print(a.upper)
+def big_letter():
+    a = input()
+    """ ввод чего-либо """
+    print(a.upper())
+
+def one_big_latter():
+    a = input()
+    """" ввод слов и вывод первых заглавных"""
+    print(a.title())
+
+one_big_latter()
